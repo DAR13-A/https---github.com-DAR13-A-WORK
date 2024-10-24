cdc95b37039036ae1d0fb1bcda9b69a1fd81d213
Family Dudkevych <arkadii@Mac-mini-Family.local>
2024-10-24 20:09:16 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let lesonNumber = 1;
+let homeWork = 81;
+let myhomeWorks = 2;
+let thusday = 12;
+
+alert('today leson number' + lesonNumber + myhomeWorks);
+alert('home work' + homeWork + lesonNumber + lesonNumber);
+console.log('thusday' + homeWork + thusday+lesonNumber +myhomeWorks);
